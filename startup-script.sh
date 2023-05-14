2ea194e694567057f3f1dfcd877810d50f2483e6
belabuda <127694375+belabuda@users.noreply.github.com>
2023-05-14 21:09:34 +0300

    Create startup-script.sh

---
+++ startup-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ruby_install_script='
+#!/bin/bash
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+'
+
+mongodb_install_script='
+#!/bin/bash
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+'
+
+deploy_script='
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y git
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+'
+
+bash -c "$ruby_install_script"
+bash -c "$mongodb_install_script"
+bash -c "$deploy_script"
